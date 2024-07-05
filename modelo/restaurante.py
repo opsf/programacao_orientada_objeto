d5d521477e9d600f381030d73f6a8d168fc5dfb5
Orlando <cursosopsf@gmail.com>
2024-07-05 11:11:55 -0300

    projeto inicial

---
+++ modelo/restaurante.py
@@ -0,0 +1,9 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restarante_comedere = Restaurante()
+restarante_comedere.nome= 'lainios clube'
+restarante_comedere.categoria='goumert'
+print(restarante_comedere.nome)
