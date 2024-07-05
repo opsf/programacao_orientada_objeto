4e4da35d13f345fddf723db2ee59fe0da5fdb182
Orlando <cursosopsf@gmail.com>
2024-07-05 11:56:55 -0300

    criando função listar restaurantes

--- modelo/restaurante.py
+++ modelo/restaurante.py
@@ -1,14 +1,19 @@
 class Restaurante:
+    restaurantes=[]
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def lista_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
         
 restarante_comedere = Restaurante('Comedere', 'Goumert')
 restaurante_pizza = Restaurante('Pizza', 'Italiana')
 
-print(restarante_comedere)
-print(restaurante_pizza)
+Restaurante.lista_restaurantes()
