7a02914cd1217073ea6b1cd9e7746a83347d8023
Orlando <cursosopsf@gmail.com>
2024-07-05 14:46:41 -0300

    listando os atributos da classe sem usar __str__

--- modelo/restaurante.py
+++ modelo/restaurante.py
@@ -6,12 +6,13 @@ class Restaurante:
         self.ativo = False
         Restaurante.restaurantes.append(self)
     
-    def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+    #def __str__(self):
+       # return f'{self.nome} | {self.categoria}'
     
-    def lista_restaurantes():
+    def lista_restaurantes():        
         for restaurante in Restaurante.restaurantes:
             print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+         
         
 restarante_comedere = Restaurante('Comedere', 'Goumert')
 restaurante_pizza = Restaurante('Pizza', 'Italiana')
