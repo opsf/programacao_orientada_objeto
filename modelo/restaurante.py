48cf8528a9bf3665e066aebb874782bf50e14e95
Orlando <cursosopsf@gmail.com>
2024-07-05 11:22:07 -0300

    imprime os objetos criados pela classe

--- modelo/restaurante.py
+++ modelo/restaurante.py
@@ -1,9 +1,10 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restarante_comedere = Restaurante()
-restarante_comedere.nome= 'lainios clube'
-restarante_comedere.categoria='goumert'
-print(restarante_comedere.nome)
+restarante_comedere = Restaurante('Comedere', 'Goumert')
+restaurante_pizza = Restaurante('Pizza', 'Italiana')
+
+print(restarante_comedere,restaurante_pizza)
